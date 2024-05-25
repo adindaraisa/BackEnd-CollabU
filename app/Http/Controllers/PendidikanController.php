bdcb145e642f23e9fbeea63a6302b5464d3f3846
adindaraisa <adindazhr11@gmail.com>
2024-05-26 06:48:11 +0700

    push foto profil

--- app/Http/Controllers/PendidikanController.php
+++ app/Http/Controllers/PendidikanController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Profil;
 use App\Models\Pendidikan;
+use App\Models\Pengguna;
 use Illuminate\Http\Request;
 
 class PendidikanController extends Controller
@@ -23,12 +24,17 @@ class PendidikanController extends Controller
         ]);
 
         // Buat koordinator baru
-        Pendidikan::create([
+        $pendidikan = Pendidikan::create([
             'id_profil' => $profil->id_profil,
             'id_jurusan' => $request->id_jurusan,
             'id_prodi' => $request->id_prodi,
             'tahun_masuk' => $request->tahun_masuk,
         ]);
+
+        $pengguna = Pengguna::find($$profil->id_pengguna);
+        $pengguna->id_jurusan = $pendidikan->id_jurusan;
+        $pengguna->id_prodi = $pendidikan->id_prodi;
+        $pengguna->save();
 
         return response()->json(['message' => 'Pendidikan berhasil dibuat']);
     }
@@ -39,6 +45,16 @@ class PendidikanController extends Controller
             return response()->json(['error' => 'Profile not found'], 404);
         }
         return response()->json($profil, 200);
+    }
+
+    public function editPendidikan($id) {
+        $profil = Profil::find($id);
+
+        if (!$profil) {
+            return response()->json(['message' => 'Profil tidak ditemukan'], 404);
+        }
+        
+        
     }
 
 }
