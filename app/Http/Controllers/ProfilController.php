310486984641f0ff6e88f152c250f02fcdb0e185
adi94958 <117360640+adi94958@users.noreply.github.com>
2024-05-31 15:07:00 +0700

    ubah return api cek kelengkapan

--- app/Http/Controllers/ProfilController.php
+++ app/Http/Controllers/ProfilController.php
@@ -12,8 +12,9 @@ use Illuminate\Http\Request;
 
 class ProfilController extends Controller
 {
-    public function getProfil($id_profil){
-        $profil = Profil::with(['pengguna','pendidikan.prodi', 'pendidikan.jurusan.perguruantinggi', 'prestasi', 'pengalaman', 'keahlian'])->find($id_profil);
+    public function getProfil($id_profil)
+    {
+        $profil = Profil::with(['pengguna', 'pendidikan.prodi', 'pendidikan.jurusan.perguruantinggi', 'prestasi', 'pengalaman', 'keahlian'])->find($id_profil);
         if (!$profil) {
             return response()->json(['error' => 'Profile not found'], 404);
         }
@@ -21,20 +22,22 @@ class ProfilController extends Controller
     }
 
 
-    public function getDaftarProfil(){
-        $profil = Profil::with(['pengguna','pendidikan.prodi', 'pendidikan.jurusan', 'prestasi', 'pengalaman', 'keahlian'])->get();
+    public function getDaftarProfil()
+    {
+        $profil = Profil::with(['pengguna', 'pendidikan.prodi', 'pendidikan.jurusan', 'prestasi', 'pengalaman', 'keahlian'])->get();
 
         return response()->json($profil, 200);
     }
-    
 
 
-    public function tambahTentangSaya(Request $request, $id){
+
+    public function tambahTentangSaya(Request $request, $id)
+    {
         $profil = Profil::find($id);
 
         if (!$profil) {
             return response()->json(['message' => 'Profil tidak ditemukan'], 404);
-        } 
+        }
 
         $request->validate([
             'tentang_saya' => 'required',
@@ -46,37 +49,32 @@ class ProfilController extends Controller
         return response()->json(['message' => 'Tentang saya berhasil ditambahkan']);
     }
 
-    public function cekKelengkapanProfil($id) {
+    public function cekKelengkapanProfil($id)
+    {
         $pengguna = Pengguna::find($id);
-    
+
         if (!$pengguna) {
             return response()->json(['error' => 'Pengguna not found'], 404);
         }
-    
+
         $profil = Profil::where('id_pengguna', $pengguna->id_pengguna)->first();
-    
+
         if (!$profil) {
             return response()->json(['error' => 'Profile not found'], 404);
         }
-    
+
         $pendidikan = Pendidikan::where('id_profil', $profil->id_profil)->get();
         $keahlian = Keahlian::where('id_profil', $profil->id_profil)->get();
         $prestasi = Prestasi::where('id_profil', $profil->id_profil)->get();
         $pengalaman = Pengalaman::where('id_profil', $profil->id_profil)->get();
         $resume = $profil->resume;
         $tentang_saya = $profil->tentang_saya;
-    
+
         // Mengecek kelengkapan profil
         if ($pendidikan->isNotEmpty() && $keahlian->isNotEmpty() && $prestasi->isNotEmpty() && $pengalaman->isNotEmpty() && !empty($resume) && !empty($tentang_saya)) {
-            return response()->json(['message' => 'Profil Lengkap']);
+            return response()->json(['message' => true]);
         }
-    
-        return response()->json(['message' => 'Profil tidak lengkap']);
+
+        return response()->json(['message' => false]);
     }
-    
-    
-
-    
 }
-
-
