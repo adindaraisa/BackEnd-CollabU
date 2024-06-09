9e1789f49095718c4226872a34643969b8a7f1ac
adi94958 <117360640+adi94958@users.noreply.github.com>
2024-06-09 20:22:56 +0700

    UPDATE: data LowonganSeeder, data PelamarSeeder,

--- database/seeders/PelamarSeeder.php
+++ database/seeders/PelamarSeeder.php
@@ -16,47 +16,49 @@ class PelamarSeeder extends Seeder
         DB::table('pelamar')->insert([
             [
                 'id_lowongan' => 1,
-                'id_pengguna' => 1,
-            ],
-            [
-                'id_lowongan' => 1,
                 'id_pengguna' => 2,
+                'created_at' => now()->subDays(2),
             ],
             [
                 'id_lowongan' => 1,
                 'id_pengguna' => 3,
+                'created_at' => now()->subDays(3),
             ],
             [
                 'id_lowongan' => 2,
                 'id_pengguna' => 1,
+                'created_at' => now()->subDays(4),
+
             ],
             [
                 'id_lowongan' => 2,
                 'id_pengguna' => 3,
+                'created_at' => now()->subDays(5),
             ],
             [
                 'id_lowongan' => 2,
                 'id_pengguna' => 5,
+                'created_at' => now()->subDays(6),
             ],
             [
                 'id_lowongan' => 3,
                 'id_pengguna' => 2,
+                'created_at' => now()->subDays(7),
             ],
             [
                 'id_lowongan' => 3,
                 'id_pengguna' => 4,
-            ],
-            [
-                'id_lowongan' => 4,
-                'id_pengguna' => 1,
+                'created_at' => now()->subDays(8),
             ],
             [
                 'id_lowongan' => 5,
                 'id_pengguna' => 1,
+                'created_at' => now()->subDays(10),
             ],
             [
                 'id_lowongan' => 6,
                 'id_pengguna' => 2,
+                'created_at' => now()->subDays(11),
             ],
         ]);
     }
